265484316a696cca91a2cd314cb68c1a6fe45eda
CodingClassesHave2ManyInstallations <bergman.z@wustl.edu>
2026-04-15 13:40:45 -0500

    Studio 9 Partially Complete

--- src/game/Projectile.java
+++ src/game/Projectile.java
@@ -3,28 +3,21 @@ package game;
 import java.awt.Color;
 
 public class Projectile extends Entity {
-
+    private double ySpeed = 0.01;
     public Projectile(double x, double y, Color color) {
         super(x, y, 0.01, color);
     }
 
     public void moveUp() {
-        setYPosition(this.getYPosition() + 0.01);
+        setYPosition(this.getYPosition() + ySpeed);
     }
 
     public void moveDown() {
-        setYPosition(this.getYPosition() - 0.01);
+        setYPosition(this.getYPosition() - ySpeed);
     }
 
     public boolean isOutOfBounds() {
-        if(this.getYPosition() > 1) {
-            return true;
-        }
-        if(this.getYPosition() < 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return ((this.getYPosition() > 1) || (this.getYPosition() < 0));
     }
 
 }
